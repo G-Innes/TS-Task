b68cb5a1190b1b34e7180dc7f793c7fbcd8f5e4a
Grant Innes <129431946+nonProphet-90@users.noreply.github.com>
2024-01-24 12:51:59 +0200

    Added more tests to cover requirements

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,8 @@
 import { Item, GildedRose } from '@/gilded-rose';
+import exp from 'constants';
 
 /**
- * Added tests for the requirements for before refactoring the gilded-rose.ts file
- * Backstage passes test is not working, i believe the test logic aligns with the requirements,
- * will come back to this one after refactoring the code.
+ * Unit tests to cover Guilded Rose requirements.
 **/
 
 describe('Gilded Rose', () => {
@@ -13,7 +12,7 @@ describe('Gilded Rose', () => {
     expect(items[0].name).toBe('foo'); // Changed expected text to match the actual text.
   });
 
-  it('should decrease quality & sellIn for normal items', () => {
+  it('should decrease quality & sellIn by 1 for normal items', () => {
     const gildedRose = new GildedRose([new Item('Normal Item', 10, 10)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(9);
@@ -53,6 +52,47 @@ describe('Gilded Rose', () => {
     // SellIn 5 days, quality increase by 3
     expect(gildedRose.items[0].sellIn).toBe(4);
     expect(gildedRose.items[0].quality).toBe(24);
+
+    // Quality drops to 0 after the concert
+    for (let i = 4; i > -1; i--) {
+      gildedRose.updateQuality();
+    }
+    expect(gildedRose.items[0].sellIn).toBe(-1);
+    expect(gildedRose.items[0].quality).toBe(0);
   });
 
+  it('should decrease quality by 2 for Conjured items', () => {
+    const gildedRose = new GildedRose([new Item('Conjured Item', 10, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(8);
+  });
+
+  it('should decrease quality by 4 for Conjured items with sellIn < 0', () => {
+    const gildedRose = new GildedRose([new Item('Conjured Item', 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(6);
+  });
+
+  it('should enure the quality of an item is never negative', () => {
+    const gildedRose = new GildedRose([new Item('Item', 10, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('should enure the quality of an item is never more than 50', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it('should enure the quality & sellIn of Sulfuras never changes', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 100, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(100);
+    expect(items[0].quality).toBe(80);
+  });
 });
