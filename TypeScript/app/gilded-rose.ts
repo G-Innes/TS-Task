d0e2cfdc9b58d2ddc7fb680f5239f9c3553d1e97
Grant Innes <129431946+nonProphet-90@users.noreply.github.com>
2024-01-24 12:09:58 +0200

    Refactored code with comments

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,4 @@
+// This class represents an item in the inventory
 export class Item {
   name: string;
   sellIn: number;
@@ -10,6 +11,7 @@ export class Item {
   }
 }
 
+// This class represents the inventory
 export class GildedRose {
   items: Array<Item>;
 
@@ -17,53 +19,78 @@ export class GildedRose {
     this.items = items;
   }
 
+  // Method for updating quality and sellIn for items in the inventory.
+  // It loops over each item in the array and calls the specificfunction for each item to update the quality.
+  // After updating the quality it decreases the sellIn by 1 & returns the updated array of items.
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
+    this.items.forEach(item => {
 
+      if (item.name === 'Aged Brie') {
+        this.updateAgedBrie(item);
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        this.updateBackstagePass(item);
+      } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        item.quality = 80; // Sulfuras quality is always 80
+      } else if (item.name.startsWith('Conjured')) {
+        this.updateConjuredItem(item);
+      } else {
+        this.updateNormalItem(item);
+      }
+      // Update all items' sellIn by 1, except for legendary items
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn--;
+      }
+      // Ensure quality never exceeds 50 for all non-legendary items
+      if (item.name !== 'Sulfuras, Hand of Ragnaros' && item.quality > 50) {
+        item.quality = 50;
+      }
+      // Ensure quality is never negative
+      if (item.quality < 0) {
+        item.quality = 0;
+      }
+    });
+    // Return updated items
     return this.items;
+  }
+
+  // Method for updating quality of normal items.
+  updateNormalItem(item: Item) {
+    if (item.quality > 0) {
+      item.quality--;
+    }
+    // Decreases quality by 1 again if the sellIn date is less than 0.
+    if (item.sellIn <= 0 && item.quality > 0) {
+      item.quality--;
+    }
+  }
+
+  // Method for updating quality of Brie- quality increases by 1 if the sellIn date > 0.
+  updateAgedBrie(item: Item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+
+  // Method for updating quality of Backstage passes.
+  updateBackstagePass(item: Item) {
+    if (item.sellIn <= 0) {
+      item.quality = 0; // Set quality to 0 if sellIn = 0.
+    } else if (item.sellIn <= 5) {
+      item.quality += 3; // Increase quality by 3 if sellIn <= 5.
+    } else if (item.sellIn <= 10) {
+      item.quality += 2; // Increase quality by 2 if sellIn <= 10.
+    } else {
+      item.quality += 1; // Otherwise, increase quality by 1.
+    }
+  }
+
+  // Method for updating quality of Conjured items.
+  updateConjuredItem(item: Item) {
+    if (item.quality > 0) {
+      item.quality -= 2; // Decrease quality by 2 as standard
+    }
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality =- 4; // Decrease quality by 4 if sellIn < 0.
+    }
   }
 }
